f20f030c3ec357eb7c25ee280c5530584ff885a3
isaac <baa44422@gmail.com>
2021-06-26 16:45:01 +0800

    Initial Commit

---
+++ Friends/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Friends
+//
+//  Created by Isaac Lee Jing Zhi on 26/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Isaac Lee Jing Zhi on 26/6/21.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
